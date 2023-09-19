7105c806926906f7219a69518b254d4901a7d345
sofiiakiryk <107884647+sofiiakiryk@users.noreply.github.com>
2023-09-19 19:05:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const employees = {
+  john: 15,
+  ann: 10,
+  alex: 30,
+};
+function findBestEmployee(employees) {
+  let bestEmployee = "";
+  let maxTasks = 0;
+
+  for (const [employee, tasksCompleted] of Object.entries(employees)) {
+    if (tasksCompleted > maxTasks) {
+      maxTasks = tasksCompleted;
+      bestEmployee = employee;
+    }
+  }
+  return bestEmployee;
+}
+function countTotalSalary(employees) {
+  const salaries = Object.values(employees);
+  const totalSalary = salaries.reduce((acc, salary) => acc + salary, 0);
+  return totalSalary;
+}
+const user = { hobby: "diving", premium: "true" };
+user.mood = " happy";
+user.hobby = " skydiving";
+user.premium = " false";
+console.log(Object.entries(user));
+for (const iterator of user) {
+  console.log(iterator);
+  return `${Object.keys(iterator)}: ${iterator}`;
+}
